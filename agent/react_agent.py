6a8a21ca976682b8721a11ef836dc04fd483a059
caosheng-hub <1135324924@qq.com>
2026-04-21 22:32:49 +0800

    Add files via upload

---
+++ agent/react_agent.py
@@ -0,0 +1,37 @@
+import sys
+import os
+# 把当前文件所在目录向上找，加到Python搜索路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 当前文件在 agent/ 目录，向上一层就是项目根目录
+project_root = os.path.dirname(current_dir)
+sys.path.append(project_root)
+from langchain.agents import create_agent
+from model.factory import chat_model
+from utils.prompt_loader import load_system_prompts
+from agent.tools.agent_tools import (rag_summarize, get_weather, get_user_location, get_user_id,
+                                     get_current_month, fetch_external_data, fill_context_for_report)
+from agent.tools.middleware import monitor_tool,log_before_model,report_prompt_switch
+class ReactAgent:
+    def __init__(self):
+        self.agent = create_agent(
+            model=chat_model,
+            system_prompt=load_system_prompts(),
+            tools=[rag_summarize,get_weather,get_user_location,get_user_id,
+                   get_current_month,fetch_external_data,fill_context_for_report],
+            middleware=[monitor_tool,log_before_model,report_prompt_switch]
+        )
+    def execute_stream(self,query: str):
+        input_dict = {
+            "messages":[
+                {"role": "user", "content": query}
+            ]
+        }
+        # 第三个参数context就是上下文runtime中的信息，就是做提示词切换的标记
+        for chunk in self.agent.stream(input_dict,stream_mode='values',context={"report":False}):
+            latest_message = chunk['messages'][-1]
+            if latest_message.content:
+                yield latest_message.content.strip() + '\n'
+if __name__ == '__main__':
+    agent = ReactAgent()
+    for chunk in agent.execute_stream('给我生成我的使用报告'):
+        print(chunk,end='',flush=True)
